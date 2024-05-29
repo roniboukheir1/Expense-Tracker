8a7650e0f8de6ca5780ca6995c70de6f825c71b9
roniboukheir1 <roni.boukheir@lau.edu>
2024-05-29 20:01:31 +0300

    Frontend<> Added the views for Category - Added Index view - Added Create view - Added Edit view

--- Controllers/TransactionController.cs
+++ Controllers/TransactionController.cs
@@ -1,36 +1,34 @@
 ﻿using Expense_Tracker.Data;
 using Expense_Tracker.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Expense_Tracker.Services;
 
 namespace Expense_Tracker.Controllers
 {
-    public class CategoryController : Controller
+    public class TransactionController : Controller
     {
         private readonly ApplicationDbContext _context;
         private readonly BreadcrumbService _breadcrumbService;
 
-        public CategoryController(ApplicationDbContext context, BreadcrumbService breadcrumbService)
+        public TransactionController(ApplicationDbContext context, BreadcrumbService breadcrumbService)
         {
             _context = context;
             _breadcrumbService = breadcrumbService;
         }
 
-        // GET: Category
         public IActionResult Index()
         {
-            var breadcrumbs = _breadcrumbService.GetBreadcrumbs("Category", "Index", "Index");
+            var breadcrumbs = _breadcrumbService.GetBreadcrumbs("Transaction", "Index", "Index");
             ViewBag.Breadcrumbs = breadcrumbs;
-            List<Category> categories = _context.Categories.ToList();
-            return View(categories);
+            List<Transaction> transactions = _context.Transactions.ToList();
+            return View(transactions);
         }
 
         // GET: Category/Create
         [HttpGet]
         public IActionResult Create()
         {
-            var breadcrumbs = _breadcrumbService.GetBreadcrumbs("Category", "Create", "Create");
+            var breadcrumbs = _breadcrumbService.GetBreadcrumbs("Transaction", "Create", "Create");
             ViewBag.Breadcrumbs = breadcrumbs;
 
             return View(new Category());
@@ -39,19 +37,18 @@ namespace Expense_Tracker.Controllers
         // POST: Category/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create(Category category)
+        public IActionResult Create(Transaction transaction)
         {
             if (ModelState.IsValid)
             {
 
-                _context.Add(category);
+                _context.Add(transaction);
                 _context.SaveChanges();
                 return RedirectToAction(nameof(Index));
             }
             return View();
         }
 
-        // GET: Category/Edit/5
         [HttpGet]
         public IActionResult Edit(int? id)
         {
@@ -59,36 +56,36 @@ namespace Expense_Tracker.Controllers
             {
                 return NotFound();
             }
-            var breadcrumbs = _breadcrumbService.GetBreadcrumbs("Category", "Edit", "Edit");
+            var breadcrumbs = _breadcrumbService.GetBreadcrumbs("Transaction", "Edit", "Edit");
             ViewBag.Breadcrumbs = breadcrumbs;
-            Category? category = _context.Categories.Find(id);
-            if (category == null)
+            Transaction? transaction= _context.Transactions.Find(id);
+            if (transaction == null)
             {
                 return NotFound();
             }
-            return View(category);
+            return View(transaction);
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Edit(Category category)
+        public IActionResult Edit(Transaction transaction)
         {
             if (ModelState.IsValid)
             {
-                _context.Update(category);
+                _context.Update(transaction);
                 _context.SaveChanges();
                 return RedirectToAction(nameof(Index));
             }
-            return View(category);
+            return View(transaction);
         }
 
         public IActionResult Delete(int id)
         {
-            Category? category = _context.Categories.Find(id);
-            if (category == null)
+            Transaction? transaction= _context.Transactions.Find(id);
+            if (transaction == null)
             {
                 return NotFound();
             }
-            _context.Categories.Remove(category);
+            _context.Transactions.Remove(transaction);
             _context.SaveChanges();
             return RedirectToAction("Index", "Category");
         }
